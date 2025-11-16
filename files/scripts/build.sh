1e2a828c0a72d75fecefcb606ec2d2c8f3705e1e
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-11-15 20:05:25 -0500

    Update and rename example.sh to build.sh

--- files/scripts/build.sh
+++ files/scripts/build.sh
@@ -5,6 +5,4 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+hyprpm add https://github.com/outfoxxed/hy3

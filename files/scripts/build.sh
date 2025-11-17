49d954e608ebdf5dc9c08a19ffe5e0caf7897b6f
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-11-16 20:45:46 -0500

    Update build.sh

--- files/scripts/build.sh
+++ files/scripts/build.sh
@@ -5,8 +5,22 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+ls -lha
+
+SDDM_CONF=$(cat <<EOF
+[Autologin]
+Session=${DESKTOP_ENV}
+User=${USERNAME}
+EOF
+)
+
+DESKTOP_ENV="i3"
+USERNAME="ryan"
+echo "${SDDM_CONF}" > /usr/etc/sddm.conf
+
 curl -sSL https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux-amd64 -o /usr/bin/jq && chmod +x /usr/bin/jq
 curl -sSL https://github.com/mikefarah/yq/releases/download/v4.48.2/yq_linux_amd64 -o /usr/bin/yq && chmod +x /usr/bin/yq
+curl -sSL https://github.com/KaranGauswami/socks-to-http-proxy/releases/download/v0.5.0/sthp-linux -o /usr/bin/sthp && chmod +x /usr/bin/sthp
 
 PIPS=$(cat <<EOF
 pulsemixer

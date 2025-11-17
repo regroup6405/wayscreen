0c09aa7251e351f43c830993ddc12cb865c81dd1
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-11-16 22:37:38 -0500

    Update onboot.sh

--- files/system/usr/bin/onboot.sh
+++ files/system/usr/bin/onboot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 echo "---> onboot start"
+
+cp /usr/etc/X11/xorg.conf.d/20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
+if lspci | grep -q -i 'AMD.*Radeon'; then
+  sed -i 's|^#||g' /etc/X11/xorg.conf.d/20-amdgpu.conf
+fi
 
 echo "---> onboot complete"
 

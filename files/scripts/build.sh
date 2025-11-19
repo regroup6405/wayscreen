c331fbdec2e993861a21dfb64151d49a27e5267e
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-11-18 22:04:27 -0500

    Update build.sh

--- files/scripts/build.sh
+++ files/scripts/build.sh
@@ -24,6 +24,14 @@ curl -sSL https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux-amd64
 curl -sSL https://github.com/mikefarah/yq/releases/download/v4.48.2/yq_linux_amd64 -o /usr/bin/yq && chmod +x /usr/bin/yq
 curl -sSL https://github.com/KaranGauswami/socks-to-http-proxy/releases/download/v0.5.0/sthp-linux -o /usr/bin/sthp && chmod +x /usr/bin/sthp
 
+CHROME_VERSION=$(google-chrome --version)
+
+curl -sSL "https://storage.googleapis.com/chrome-for-testing-public/${CHROME_VERSION}/linux64/chromedriver-linux64.zip" -o chromedriver-linux64.zip \
+  && unzip chromedriver-linux64.zip \
+  && mv chromedriver-linux64/chromedriver /usr/bin/chromedriver \
+  && chmod +x /usr/bin/chromedriver \
+  && rm -rf chromedriver-linux64
+
 PIPS=$(cat <<EOF
 pulsemixer
 python-mpv

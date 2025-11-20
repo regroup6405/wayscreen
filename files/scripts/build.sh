a038ea7527e7a7008b638d8ba9058d39975177f2
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-11-19 21:20:21 -0500

    Update build.sh

--- files/scripts/build.sh
+++ files/scripts/build.sh
@@ -20,6 +20,8 @@ EOF
 
 echo "${SDDM_CONF}" > /etc/sddm.conf
 
+ln -sf /usr/lib/systemd/system/sddm.service /etc/systemd/system/display-manager.service
+
 curl -sSL https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux-amd64 -o /usr/bin/jq && chmod +x /usr/bin/jq
 curl -sSL https://github.com/mikefarah/yq/releases/download/v4.48.2/yq_linux_amd64 -o /usr/bin/yq && chmod +x /usr/bin/yq
 curl -sSL https://github.com/KaranGauswami/socks-to-http-proxy/releases/download/v0.5.0/sthp-linux -o /usr/bin/sthp && chmod +x /usr/bin/sthp
